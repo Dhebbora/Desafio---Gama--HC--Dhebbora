2104c26155688ca0e652d1d7cc92f244993e5fe3
Dhebbora <45688600+Dhebbora@users.noreply.github.com>
2021-07-18 20:54:59 -0300

    Subindo arquivos alterados

--- Desafio GAMA - HC - Dhebbora/script.js
+++ Desafio GAMA - HC - Dhebbora/script.js
@@ -1,7 +1,15 @@
 $('#myModal').on('shown.bs.modal', function () {
   $('#myInput').focus()
-})
+});
 
 $('#myModal').modal({
   keyboard: false
-})
+});
+
+
+function handleChange() {
+  const saveDados = [];
+  saveDados.push(localStorage.getItem('emailCadastro'));
+  saveDados.push(emailChave.value);
+  localStorage.setItem('emailCadastro', saveDados);
+};
